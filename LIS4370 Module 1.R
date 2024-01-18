afcdba79ca2e496acb64088fefc0125870c35cda
nbaumeister2000 <155272680+nbaumeister2000@users.noreply.github.com>
2024-01-17 20:34:59 -0500

    Add files via upload

---
+++ LIS4370 Module 1.R
@@ -0,0 +1,9 @@
+#Sample data
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+#Original function
+myMean <- function(assignment2) { 
+  return(sum(assignment2)/length(assignment2)) 
+}
+
+myMean(assignment2)
