f4710eee6c90c976c1cec11ed4195f71253b6276
nbaumeister2000 <155272680+nbaumeister2000@users.noreply.github.com>
2024-02-10 22:17:34 -0500

    Add files via upload

---
+++ LIS 4370 M5.R
@@ -0,0 +1,15 @@
+#Find the value of inverse of a matrix, determinant of a matrix by using the following values:
+A = matrix(1:100, nrow=10)
+B <- matrix(1:1000, nrow=10)
+
+#Print data
+A
+B
+
+#Find determinants
+det(A)
+det(B)
+
+#inverse
+t(A)
+t(B)
